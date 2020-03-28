fb466561da5b756e114319d10cb8e6ee5925e49b
Persjanow Jakub <jakubpersjanow@gmail.com>
2020-03-28 02:00:46 +0100

    first commit for ex3

---
+++ ex3_decorators.py
@@ -0,0 +1,103 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+from random import randrange
+
+class Keyboard(ABC):
+    #available keys to update and cycle through
+    available_keys = ["a", "b", "c", "d"]
+    key_pressed = None
+    _observers: List[Key] = []
+    instance = None
+
+
+    @staticmethod
+    def getInstance():
+        if Keyboard.instance == None:
+            Keyboard()
+        return Keyboard()
+
+    def __init__(self):
+        if Keyboard.instance != None:
+            raise Exception("This is a singleton class")
+        else:
+            Keyboard.instance = self
+
+    def attach(self, observer: Key) -> None:
+        print("Attached")
+        self._observers.append(observer)
+
+    def detach(self, observer: Key) -> None:
+        print("Detached")
+        self._observers.remove(observer)
+
+    def notify(self) -> None:
+        print("Notifying observers")
+        for observer in self._observers:
+            observer.update(self)
+
+    def change_key(self) -> None:
+        print("Changing key")
+        self.key_pressed = self.available_keys[randrange(0, len(self.available_keys))]
+
+        print(f"Pressed key is now {self.key_pressed}")
+        self.notify()
+
+class Key(ABC):
+    def __init__(self, key: str):
+        self.key = key
+
+    def update(self, subject: Keyboard) -> None:
+        if subject.key_pressed == self.key:
+            print(f"Key {self.key} pressed")
+            subject.detach(self)
+
+class Decorator1(Key):
+    _key: Key = None
+
+    def __init__(self, key: Key) -> None:
+        self._key = key
+
+    @property
+    def key(self):
+        return self._key
+
+    def update(self, subject: Keyboard) -> None:
+        if subject.key_pressed == self.key.key:
+            print(f"Key {self.key.key} pressed!")
+            subject.detach(self)
+
+
+class Decorator2(Key):
+    _key: Key = None
+
+    def __init__(self, key: Key) -> None:
+        self._key = key
+
+    @property
+    def key(self):
+        return self._key
+
+    def update(self, subject: Keyboard) -> None:
+        if subject.key_pressed == self.key.key:
+            print(f"Key {self.key.key} pressed?")
+            subject.detach(self)
+
+
+
+if __name__ == "__main__":
+    keyboard = Keyboard()
+    k1 = Key("a")
+    k2 = Key("b")
+    k3 = Decorator1(Key("c"))
+    k4 = Decorator2(Key("d"))
+    keyboard.attach(k1)
+    keyboard.attach(k2)
+    keyboard.attach(k3)
+    keyboard.attach(k4)
+
+    while keyboard._observers:
+        keyboard.change_key()
+
+
+
