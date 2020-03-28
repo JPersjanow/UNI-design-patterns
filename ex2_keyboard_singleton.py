15a6382748459852b3f0d7d83b8ef08be9155e5c
Persjanow Jakub <jakubpersjanow@gmail.com>
2020-03-28 02:20:42 +0100

    updated ex2 file (some wierd problem)

--- ex2_keyboard_singleton.py
+++ ex2_keyboard_singleton.py
@@ -1,18 +1,67 @@
-class Singleton(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+from random import randrange
 
-class Keyboard(metaclass=Singleton):
-    def __init__(self, key_pressed: str):
-        self.key_pressed = key_pressed
+class Keyboard(ABC):
+    #available keys to update and cycle through
+    available_keys = ["a", "b", "c", "d"]
+    key_pressed = None
+    _observers: List[Key] = []
+    instance = None
 
-class Key:
-    def __init__(self, key):
+
+    @staticmethod
+    def getInstance():
+        if Keyboard.instance == None:
+            Keyboard()
+        return Keyboard()
+
+    def __init__(self):
+        if Keyboard.instance != None:
+            raise Exception("This is a singleton class")
+        else:
+            Keyboard.instance = self
+
+    def attach(self, observer: Key) -> None:
+        print("Attached")
+        self._observers.append(observer)
+
+    def detach(self, observer: Key) -> None:
+        print("Detached")
+        self._observers.remove(observer)
+
+    def notify(self) -> None:
+        print("Notifying observers")
+        for observer in self._observers:
+            observer.update(self)
+
+    def change_key(self) -> None:
+        print("Changing key")
+        self.key_pressed = self.available_keys[randrange(0, len(self.available_keys))]
+
+        print(f"Pressed key is now {self.key_pressed}")
+        self.notify()
+
+class Key(ABC):
+    def __init__(self, key: str):
         self.key = key
 
+    def update(self, subject: Keyboard) -> None:
+        if subject.key_pressed == self.key:
+            print(f"Key {self.key} pressed")
+            subject.detach(self)
 
+if __name__ == "__main__":
+    keyboard = Keyboard()
+    k1 = Key("a")
+    k2 = Key("b")
+    k3 = Key("c")
+    k4 = Key("d")
+    keyboard.attach(k1)
+    keyboard.attach(k2)
+    keyboard.attach(k3)
+    keyboard.attach(k4)
 
-
+    while keyboard._observers:
+        keyboard.change_key()
